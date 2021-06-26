a4ddaa8332db1f26e52e623f65f9dd12d6b6d634
AlexanderHBauer <alexander.heinr.bauer@gmail.com>
2021-06-26 15:00:07 +0200

    Expanded css.

--- MyReactApp/src/MiniSlide.js
+++ MyReactApp/src/MiniSlide.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function MiniSlide(props){
     return (
-        <div className="Slide">
+        <div className="MiniSlide">
             <div className="MiniSlideDiv center">
                 <img className="MiniSlideImg center" src={props.imgSrc} alt="couldn't load image."></img>
             </div>
